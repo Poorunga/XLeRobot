ec7abaa8cc5c52288bc304d617620423720fa1d5
Poorunga <2744323@qq.com>
2025-11-22 16:07:37 +0800

    添加仅有右臂和底盘的xlerobot控制代码

---
+++ software/src/robots/xlerobot_right_arm/__init__.py
@@ -0,0 +1,4 @@
+from .config_xlerobot_right_arm import XLerobotRightArmConfig
+from .xlerobot_right_arm import XLerobotRightArm
+# from .xlerobot_client import XLerobotClient
+# from .xlerobot_host import XLerobotHost, XLerobotHostConfig
